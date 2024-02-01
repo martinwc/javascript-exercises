32af7a60a78bd6e126be42b5199ac16967594f47
Martin Leung <20199867+martinwc@users.noreply.github.com>
2024-02-01 11:21:02 -0500

    repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let result = "";
+for(let i = 0; i < num; i++) {
+    return result += repeatString("hey",3);
+}
 };
 
 // Do not edit below this line
