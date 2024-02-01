953602168478d2ce3f839c91fed9ba207fbc177f
Martin Leung <20199867+martinwc@users.noreply.github.com>
2024-02-01 15:42:24 -0500

    reverseString.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+let reverseArray = []
+for (let i = 0; i < inputString.length; i++) {
+    reverseArray.push(inputString[i]);
+    reverseArray.reverse();
+    reverseArray.join("");
+} 
 
+return reverseString("hello there");
 };
 
 // Do not edit below this line
