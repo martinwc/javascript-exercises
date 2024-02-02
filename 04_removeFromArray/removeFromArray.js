6786f2086f26e866a6cbef4f60fbf60825b5ffd2
Martin Leung <20199867+martinwc@users.noreply.github.com>
2024-02-02 14:23:01 -0500

    removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, elementToRemove) {
+    const newArray = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== elementToRemove) {
+        newArray.push(arr[i]);
+      }
+    }
+  
+    return newArray;
+  };
 
-};
 
+// removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
 // Do not edit below this line
 module.exports = removeFromArray;
