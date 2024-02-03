7a9e10fb84a2be6e57bf3ab38403e80519fb1539
Martin Leung <20199867+martinwc@users.noreply.github.com>
2024-02-02 22:20:44 -0500

    .

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Celcius) {
+  return Math.round((Celcius - 32) * 5/9 * 10) /10;
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(Fahrenheit) {
+  return Math.round(((Fahrenheit * 9/5) + 32 ) *10 ) / 10;
+  
 };
 
 // Do not edit below this line
